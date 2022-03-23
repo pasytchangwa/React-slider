90c9d25aa1533ea5015bf0040363ecb1ed63951c
sylvestre <pasytchangwa@gmail.com>
2022-03-23 12:42:53 +0200

    Remove unwanted files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+import Review from "./Review";
 
 function App() {
   const [reviews, setReviews] = useState(data);
@@ -49,35 +48,7 @@ function App() {
             <span>/</span> Reviews
           </h2>
         </div>
-        <div className="section-center">
-          {reviews.map((review, reviewIndex) => {
-            const { id, image, name, quote, title } = review
-
-            let position = 'nextSlide'
-            if (reviewIndex === index) {
-              position = 'activeSlide'
-            }
-            if (reviewIndex === index - 1 || (index === 0 && reviewIndex === reviews.length - 1)) {
-              position = 'lastSlide'
-            }
-
-            return (
-              <article className={position} key={id}>
-                <img src={image} alt={name} className='person-img' />
-                <h4>{name}</h4>
-                <p className="title">{title}</p>
-                <p className="text">{quote}</p>
-                <FaQuoteRight className="icon"/>
-              </article>
-            )
-          })}
-          <button type="button" className="prev" onClick={handleClickLeft}>
-          <FiChevronLeft />
-          </button>
-          <button type="button" className="next" onClick={handleClickRight}>
-          <FiChevronRight />
-          </button>
-        </div>
+        <Review reviews={reviews} index={index} handleClickLeft={handleClickLeft} handleClickRight={handleClickRight}/>
       </section> 
   );
 }
